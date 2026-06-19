605a8cd7b425aab4ca2afbaf0bea677a0f05f6f9
oscar <https://github.com/oscarsekai>
2026-06-20 01:16:02 +0800

    fix Render port binding

--- server.js
+++ server.js
@@ -66,4 +66,5 @@ const requestListner = (req, res) => {
 };
 
 const server = http.createServer(requestListner);
-server.listen(3000);
+const port = process.env.PORT || 3000;
+server.listen(port, '0.0.0.0');
